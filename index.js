f2de1cc6baaf1354108d29d27445f6b44ec844de
sudipto <sudiptocsi@gmail.com>
2021-09-12 20:37:41 +0600

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
@@ -7,30 +7,59 @@ app.use(cors());
 app.use(express.json());
 // ROUTES
 // crate a todo
-app.post("/todos", async(req, res) => {
-    try {
-        const { description } = req.body;
-        const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1) RETURNING *",[description]
-        );
-        res.json(newTodo.rows[0]);
-    } catch (error) {
-        console.error(error.message);
-    }
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 // get all todos
-app.get('/todos', async(req, res) => {
-    try {
-        const todoList = await pool.query("SELECT * FROM todo ORDER BY todo_id DESC");
-        res.json(todoList.rows);
-    } catch (error) {
-        console.log(error.message);
-    }
+app.get("/todos", async (req, res) => {
+  try {
+    const todoList = await pool.query(
+      "SELECT * FROM todo ORDER BY todo_id DESC"
+    );
+    res.json(todoList.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 // get a todo
-
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const toDo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(toDo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+// update todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    if (updateTodo) {
+      res.json("updated successfully");
+    } else {
+      res.json("not updated");
+    }
+  } catch (error) {}
+});
 // delete a todo
 // server started
 app.listen(5000, () => {
-    console.log('server started');
+  console.log("server started");
 });
